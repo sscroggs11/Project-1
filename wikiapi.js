df610c126ddc53ffba00c10a8545fea412d2f857
arparent <arparent@gmail.com>
2022-05-10 20:48:36 -0700

    Added Wikipedia search api js file

---
+++ wikiapi.js
@@ -0,0 +1,4 @@
+// wikiapi.js, created by Adam Parent, 10 May 2022.
+
+var wikiapiurl = "http://api.geonames.org/wikipediaSearchJSON?q=" + cityName + "&maxRows=1&username=arparent";
+//Note: "arparent" is the required registerd username for this service.
