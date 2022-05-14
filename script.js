bd318067f6e91190fdf2491cb3b439e971f0f2fe
Samuel Scroggs <102083372+sscroggs11@users.noreply.github.com>
2022-05-14 10:29:40 -0700

    Update script.js

--- script.js
+++ script.js
@@ -3,24 +3,28 @@ var placeSearch, autocomplete, geocoder;
 function initAutocomplete() {
   geocoder = new google.maps.Geocoder();
   autocomplete = new google.maps.places.Autocomplete(
-      (document.getElementById('autocomplete'))/*,
-      {types: ['(cities)']}*/);
+    (document.getElementById('autocomplete')), {
+      types: ['geocode']
+    });
 
   autocomplete.addListener('place_changed', fillInAddress);
 }
 
 function codeAddress(address) {
-    geocoder.geocode( { 'address': address}, function(results, status) {
-      if (status == 'OK') {
-        alert(results[0].geometry.location);
-      } else {
-        alert('Geocode was not successful for the following reason: ' + status);
-      }
-    });
-  }
+  geocoder.geocode({
+    'address': address
+  }, function(results, status) {
+    if (status == 'OK') {
+      // This is the lat and lng results[0].geometry.location
+      alert(results[0].geometry.location);
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
+}
 
 function fillInAddress() {
   var place = autocomplete.getPlace();
-  alert(place.place_id);
-  //   codeAddress(document.getElementById('autocomplete').value);
+
+  codeAddress(document.getElementById('autocomplete').value);
 }
