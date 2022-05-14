add8644a1bae5ae0c916223e1e145784a7de0a24
Samuel Scroggs <102083372+sscroggs11@users.noreply.github.com>
2022-05-14 12:15:08 -0700

    Update script.js
    test to remove geocode function

--- script.js
+++ script.js
@@ -10,18 +10,18 @@ function initAutocomplete() {
   autocomplete.addListener('place_changed', fillInAddress);
 }
 
-function codeAddress(address) {
-  geocoder.geocode({
-    'address': address
-  }, function(results, status) {
-    if (status == 'OK') {
-      // This is the lat and lng results[0].geometry.location
-      alert(results[0].geometry.location);
-    } else {
-      alert('Geocode was not successful for the following reason: ' + status);
-    }
-  });
-}
+// function codeAddress(address) {
+//   geocoder.geocode({
+//     'address': address
+//   }, function(results, status) {
+//     if (status == 'OK') {
+//       // This is the lat and lng results[0].geometry.location
+//       alert(results[0].geometry.location);
+//     } else {
+//       alert('Geocode was not successful for the following reason: ' + status);
+//     }
+//   });
+// }
 
 function fillInAddress() {
   var place = autocomplete.getPlace();
