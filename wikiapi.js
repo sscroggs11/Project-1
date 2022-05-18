febce0968479f10d4c08008f519c5d851c047339
arparent <arparent@gmail.com>
2022-05-17 21:28:22 -0700

    Got the api working

--- wikiapi.js
+++ wikiapi.js
@@ -1,6 +1,6 @@
 // wikiapi.js, created by Adam Parent, 10 May 2022.
 
-var cityName = "";  //To be pulled from either input or Sam's API after validation - should be pre-trimmed
+var cityName = "Salt Lake City";  //To be pulled from either input or Sam's API after validation - should be pre-trimmed
 const wikiUsername = "arparent";  //Required for API - registered username for Adam Parent.
 var cityNameText = document.getElementById('city-name-text');       //Todo - Create this element in HTML.
 var wikiText = document.getElementById('wiki-text');                //Todo - Create this element in HTML.
@@ -8,11 +8,11 @@ var wikiText = document.getElementById('wiki-text');                //Todo - Cre
 function getWikiApi(cityName) {                         //Takes the city name and returns a matching summary from Wikipedia.
     cityNameText.textContent = cityName;                //Display the city name on the page without alteration.
     cityNameArr = cityName.split(" ");                  //Parse out any spaces in the city name - they cannot be in the URL.
-    wikiCityName = "";                                  //Creates a string for city name in the URL.
+    var wikiCityName = "";                                   //Creates a string for city name in the URL.
     for (var i = 0; i < cityNameArr.length; i++){       //Re-assembles the city name string, replacing spaces with "%20".
-        wikiCityName.concat(cityNameArr[i]);
+        wikiCityName = wikiCityName.concat(cityNameArr[i]);
         if((i + 1) < cityNameArr.length)
-            wikiCityName.concat("%20");
+            wikiCityName = wikiCityName.concat("%20");
         else
             break;
     }
@@ -25,6 +25,8 @@ function getWikiApi(cityName) {                         //Takes the city name an
         return response.json();
       })
       .then(function (data) {
-        wikiText.textContent = data.geonames.summary;                           //If no error, populate text with result.
+        wikiText.textContent = data.geonames[0].summary;                           //If no error, populate text with result.
       });
 }
+
+getWikiApi(cityName);
