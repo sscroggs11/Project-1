09fdd1e580b8fe37d32c88462014462daefdec93
arparent <arparent@gmail.com>
2022-05-20 17:17:14 -0700

    Updated to new wiki code

--- wikiapi.js
+++ wikiapi.js
@@ -1,7 +1,6 @@
 // wikiapi.js, created by Adam Parent, 10 May 2022.
 
-var cityName = "Salt Lake City";  //To be pulled from either input or Sam's API after validation - should be pre-trimmed
-// const wikiUsername = "arparent";  //Required for API - registered username for Adam Parent.
+var cityName = "Tukwila";  //To be pulled from either input or Sam's API after validation - should be pre-trimmed
 var cityNameText = document.getElementById('city-name-text');       //Todo - Create this element in HTML.
 var wikiText = document.getElementById('wiki-text');                //Todo - Create this element in HTML.
 var cityNameURL = "";
@@ -24,33 +23,19 @@ function prepCityNameforURL(){
   console.log(cityNameDisplay);
 }
 
-function getWikiApi() {
-    var wikiApiURL = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=revisions&rvprop=content&rvsection=0&titles=" + cityNameURL;   //Build the URL
-    console.log(wikiApiURL);
-    fetch(wikiApiURL)
-      .then(function (response) {
-        if (response.status < 200 || response.status >= 400) {                  //Check Status - if not in 300 range, return error message.
-          wikiText.textContent = "Response Error Code: " + response.status;     //Todo - See if API returns any status messages, append if provided.
-        }                                                                       //Todo - If no data available, "summary not available" message instead.
-        return response.json();
-      })
-      .then(function (data) {
-        // wikiText.textContent = data.geonames[0].summary;                           //If no error, populate text with result.
-        console.log(data);
-      });
-}
-
-prepCityNameforURL();
-getWikiApi();
-
 async function searchWikipedia(searchQuery) {
-  const endpoint = `https://en.wikipedia.org/w/api.php?action=query&format=json&prop=revisions&rvprop=content&rvsection=0&titles=${searchQuery}`;
+  // const endpoint = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=${searchQuery}`;
+  var headers = { 'Access-Control-Allow-Origin': 'http://127.0.0.1:5500'}
+  const endpoint = `https://gentle-brushlands-07400.herokuapp.com/http://api.geonames.org/wikipediaSearchJSON?maxRows=1&username=arparent&q=${searchQuery}`;
   const response = await fetch(endpoint);
   if (!response.ok) {
     throw Error(response.statusText);
   }
   const json = await response.json();
   console.log(json);
+  wikiText.textContent = json.geonames[0].summary;                           //If no error, populate text with result.
   return json;
 }
-searchWikipedia('seattle')
+
+prepCityNameforURL();
+searchWikipedia(cityNameDisplay);
