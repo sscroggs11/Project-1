f0640378f5f37fe3621c2c8e7411197f27bc4a15
arparent <arparent@gmail.com>
2022-05-20 19:06:11 -0700

    Code Cleanup

--- wikiapi.js
+++ wikiapi.js
@@ -6,6 +6,7 @@ var cityNameText = document.getElementById('city-name-text');
 var wikiText = document.getElementById('wiki-text');
 var leftChild = document.getElementById('left-child');
 
+//This function takes the city name, parses out the spaces and formats it for use in a URL
 function prepCityNameforURL(){
   cityNameArr = cityName.split(" ");
   cityNameURL = "";
@@ -17,6 +18,7 @@ function prepCityNameforURL(){
   }
 }
 
+//This function queries the wikipedia API and returns the results
 async function searchWikipedia() {
   const endpoint = "https://gentle-brushlands-07400.herokuapp.com/http://api.geonames.org/wikipediaSearchJSON?maxRows=1&username=arparent&q=" + cityNameURL;
   const response = await fetch(endpoint);
@@ -24,22 +26,18 @@ async function searchWikipedia() {
     throw Error(response.statusText);
   }
   const json = await response.json();
-  console.log(json);
-  cityNameText.textContent = json.geonames[0].title;
-  wikiText.textContent = json.geonames[0].summary + "  ";
+  cityNameText.textContent = json.geonames[0].title;        //Populate the city name
+  wikiText.textContent = json.geonames[0].summary + "  ";   //Populate the city summary
   var wikiLink = document.createElement("a");
-  wikiLink.textContent = json.geonames[0].title + " on Wikipedia👆";
+  wikiLink.textContent = json.geonames[0].title + " on Wikipedia👆";  
   wikiLink.setAttribute("href", ("http://" + json.geonames[0].wikipediaUrl));
   wikiLink.setAttribute("target", "_blank");
-  wikiText.append(wikiLink);
+  wikiText.append(wikiLink);                                //Create and append a link to the wiki article
   var lastCity = localStorage.getItem("lastCity");
-  if(lastCity != json.geonames[0].title){
+  if(lastCity != json.geonames[0].title){                   //Compare current city to last searched in local storage, if different display last city
     var lastCityText = document.createElement("div");
     lastCityText.textContent = "Last city searched: " + lastCity;
     leftChild.append(lastCityText);
   }
-  localStorage.setItem("lastCity", json.geonames[0].title);
-  console.log(localStorage.getItem("lastCity"));
-
-  return json;
+  localStorage.setItem("lastCity", json.geonames[0].title); //Update last city
 }
